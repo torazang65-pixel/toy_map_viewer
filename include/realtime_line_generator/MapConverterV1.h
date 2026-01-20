c0cff6b21638f1bdd07d987c3f3b83ffb50399b9
torazang65 <torazang65@gmail.com>
2026-01-20 12:22:55 +0900

    feat: introduce MapConverterV1 for single map conversion and refactor MapConverterV2 to process multiple maps.

--- include/realtime_line_generator/MapConverterV1.h
+++ include/realtime_line_generator/MapConverterV1.h
@@ -8,23 +8,24 @@
 #include "common/DataTypes.h"
 #include "LaneUtils.h"
 
-class MapConverterV2 {
+class MapConverterV1 {
 public:
-    MapConverterV2();
-    ~MapConverterV2() = default;
+    MapConverterV1();
+    ~MapConverterV1() = default;
 
     void run();
 
 private:
     void loadParameters();
-    void processMap(const std::string& input_subdir, const std::string& output_filename);
 
 private:
     ros::NodeHandle nh_;
     LaneConfig lane_config_;
     std::string package_name_;
-    std::string base_input_dir_;
-    std::string output_root_dir_;
+    std::string input_folder_name_;
+    std::string output_folder_name_;
+    std::string base_dir_;
+    std::string output_dir_;
 
     std::map<int, Lane> global_map_;
 };
